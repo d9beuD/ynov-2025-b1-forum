04036eca962659b375b7b578e420c490278c5be0
d9beuD <53569029+d9beuD@users.noreply.github.com>
2026-02-10 14:12:47 +0100

    Add Navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import NavbarItem from './NavbarItem.vue'
+</script>
+
+<template>
+  <nav class="p-4">
+    VueReddit
+    <ul>
+      <NavbarItem label="Accueil" :to="{ name: 'homepage' }" />
+      <NavbarItem label="Connexion" :to="{ name: 'login' }" />
+      <NavbarItem label="Inscription" :to="{ name: 'login' }" />
+    </ul>
+  </nav>
+</template>
---
+++ src/components/NavBarItem.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { RouteLocationAsPathGeneric, RouteLocationAsRelativeGeneric } from 'vue-router'
+
+const props = defineProps<{
+  label: string
+  to: RouteLocationAsRelativeGeneric | RouteLocationAsPathGeneric
+}>()
+</script>
+
+<template>
+  <li>
+    <RouterLink
+      class="block rounded p-2 text-slate-600 transition-colors hover:bg-slate-100 hover:text-slate-950"
+      :to="props.to"
+      >{{ props.label }}</RouterLink
+    >
+  </li>
+</template>

feb41c7209368c37110a1a727abb01500a01d1d4
d9beuD <53569029+d9beuD@users.noreply.github.com>
2026-02-10 14:41:12 +0100

    Fix typo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import NavbarItem from './NavbarItem.vue'
+import NavBarItem from './NavBarItem.vue'
 </script>
 
 <template>
   <nav class="p-4">
     VueReddit
     <ul>
-      <NavbarItem label="Accueil" :to="{ name: 'homepage' }" />
-      <NavbarItem label="Connexion" :to="{ name: 'login' }" />
-      <NavbarItem label="Inscription" :to="{ name: 'login' }" />
+      <NavBarItem label="Accueil" :to="{ name: 'homepage' }" />
+      <NavBarItem label="Connexion" :to="{ name: 'login' }" />
+      <NavBarItem label="Inscription" :to="{ name: 'login' }" />
     </ul>
   </nav>
 </template>

0ca91c67364bfb6b51b1ef57eab49fe1adb0518d
d9beuD <53569029+d9beuD@users.noreply.github.com>
2026-02-11 10:21:03 +0100

    Make NavBar reacts to auth status changes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
+import { useAuthStore } from '@/stores/auth'
 import NavBarItem from './NavBarItem.vue'
+
+const authStore = useAuthStore()
 </script>
 
 <template>
   <nav class="p-4">
     VueReddit
     <ul>
-      <NavBarItem label="Accueil" :to="{ name: 'homepage' }" />
-      <NavBarItem label="Connexion" :to="{ name: 'login' }" />
-      <NavBarItem label="Inscription" :to="{ name: 'login' }" />
+      <template v-if="authStore.isLoggedIn">
+        <NavBarItem label="Accueil" :to="{ name: 'homepage' }" />
+        <NavBarItem label="Topics" :to="{ name: 'topics' }" />
+      </template>
+      <template v-else>
+        <NavBarItem label="Connexion" :to="{ name: 'login' }" />
+        <NavBarItem label="Inscription" :to="{ name: 'login' }" />
+      </template>
     </ul>
   </nav>
 </template>
